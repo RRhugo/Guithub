c2994c946d8e6aeec039064c13e4104c9f73f8b4
HugoRamon-AlumnoCEAC <HugoRamon-AlumnoCEAC@CEACFP20462>
2023-05-12 13:20:12 +0200

    Creamos una nueva variable y un nuevo bucle

--- src/sencillo/GuitHub.java
+++ src/sencillo/GuitHub.java
@@ -3,20 +3,27 @@ public class GuitHub {
 		
 	public static void main(String[] args)
 	{
-	
-	System.out.println("Hola usuario bienvenido");
-	System.out.println("A continuación se muestra una cuenta regresiva");
-	
-	for (int i = 10; i >= 1;  i--)
-	{
-	    System.out.println(i);
-	}
-	
-	System.out.println("Ahora, otra cuenta progresiva:");
-	
-	for (int i = 10; i >= 1;  i++)
-	{
-	    System.out.print(i);
-	}
+		int x = 0;
+		
+		if (x == 0) {
+			
+			System.out.print("Hello World");
+			x--;
+		}
+		
+		System.out.println("Hola usuario bienvenido");
+		System.out.println("A continuación se muestra una cuenta regresiva");
+		
+		for (int i = 10; i >= 1;  i--)
+		{
+		    System.out.println(i);
+		}
+		
+		System.out.println("Ahora, otra cuenta progresiva:");
+		
+		for (int i = 10; i >= 1;  i++)
+		{
+		    System.out.print(i);
+		}
 }
 }

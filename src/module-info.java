68c5a790a5509c15306124b850cc3236a463566f
HugoRamon-AlumnoCEAC <HugoRamon-AlumnoCEAC@CEACFP20462>
2023-05-12 13:18:19 +0200

    Sin cambios

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author HugoRamon-AlumnoCEAC
+ *
+ */
+module Sencillo {
+}
---
+++ src/sencillo/GuitHub.java
@@ -0,0 +1,22 @@
+package sencillo;
+public class GuitHub {
+		
+	public static void main(String[] args)
+	{
+	
+	System.out.println("Hola usuario bienvenido");
+	System.out.println("A continuación se muestra una cuenta regresiva");
+	
+	for (int i = 10; i >= 1;  i--)
+	{
+	    System.out.println(i);
+	}
+	
+	System.out.println("Ahora, otra cuenta regresiva:");
+	
+	for (int i = 10; i >= 1;  i++)
+	{
+	    System.out.print(i);
+	}
+}
+}
